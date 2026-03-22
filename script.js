b850d36a4b8997ee04425e45249d0f4b6d282d23
Saurav Thakuria <99746283+s-aur-av@users.noreply.github.com>
2026-03-23 02:35:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,26 @@
-const scene = new THREE.Scene()
-
-const camera = new THREE.PerspectiveCamera(
-75,
-window.innerWidth/window.innerHeight,
-0.1,
-1000
-)
-
-const renderer = new THREE.WebGLRenderer({
-canvas:document.querySelector('#bg'),
-})
-
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth,window.innerHeight)
-
-camera.position.setZ(30)
-
-renderer.render(scene,camera)
-
-const geometry = new THREE.TorusGeometry(10,3,16,100)
-
-const material = new THREE.MeshStandardMaterial({
-color:0x38bdf8
-})
-
-const torus = new THREE.Mesh(geometry,material)
-
-scene.add(torus)
-
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(20,20,20)
-
-scene.add(pointLight)
-
-function animate(){
-
-requestAnimationFrame(animate)
-
-torus.rotation.x += 0.01
-torus.rotation.y += 0.01
-
-renderer.render(scene,camera)
-
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
 }
-console.log("Animation script loaded");
+// function handleSubmit() {
+//   document.getElementById("success-msg").innerText = "🚀 Sending...";
 
-animate()
+//   setTimeout(() => {
+//     document.getElementById("success-msg").innerText = "✅ Message sent successfully!";
+//   }, 1500);
 
-function scrollToProjects(){
-document.getElementById("projects").scrollIntoView({
-behavior:"smooth"
-})
+//   return true; // VERY IMPORTANT
+// }
+function handleSubmit() {
+  const msg = document.getElementById("success-msg");
 
+  msg.innerText = "🚀 Sending...";
+
+  setTimeout(() => {
+    msg.innerText = "✅ Message sent successfully!";
+    document.querySelector("form").reset();
+  }, 1500);
+
+  return true;
 }
